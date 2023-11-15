b0d48a898c6e4b4816980ec073c382062dae2541
MinhThach1908 <pandaminh2008@gmail.com>
2023-11-15 15:41:08 +0700

    c06

---
+++ javascript and jquery/c06/js/event-listener-with-parameters.js
@@ -0,0 +1,14 @@
+var elUsername = document.getElementById('username');
+var elMsg      = document.getElementById('feedback');
+
+function checkUsername(minLength) {
+    if (elUsername.ariaValueMax.length , minLength) {
+        elMsg.innerHTML = 'Username must be ' + minLength + ' characters or more';
+    } else {
+        elMsg.innerHTML = '';
+    }
+}
+
+elUsername.addEventListener('blur', function() {
+    checkUsername(15);
+}, false); 
