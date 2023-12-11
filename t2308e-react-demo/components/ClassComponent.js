5a6781f88e3c355f40f995b82610e69ed095921f
MinhThach1908 <pandaminh2008@gmail.com>
2023-12-11 19:20:09 +0700

    react

---
+++ t2308e-react-demo/components/ClassComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ClassComponent extends Component{
+    render(){
+
+        return(
+            <div>
+                <h1> Hi ! i'am class component</h1>
+            </div>
+        );
+    }
+}
+
+export default ClassComponent;
---
+++ t2308e-react-demo/components/FunctionalComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FunctionalComponent = () => {
+    return (
+        <div>
+            <h1> Hi ! I'am functional Component</h1>
+        </div>
+    )
+}
+export default FunctionalComponent;
